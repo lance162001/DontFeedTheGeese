adab502e4159895511576bac6d237fef58e8da2a
Lance Gruber <lancegruber2@gmail.com>
2021-11-06 00:31:29 -0400

    Removed movement acceleration

--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -7,8 +7,8 @@ public class UserInput : MonoBehaviour
     public float speed;
     bool firing = false;
     public void input(){
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
         transform.Translate(new Vector3(x,y,0)*speed*Time.deltaTime);
         if (!firing && Input.GetMouseButtonDown(0)){
             firing=true;
@@ -16,6 +16,11 @@ public class UserInput : MonoBehaviour
         if (firing){
             Attack();
         }
+    }
+    //want to put this into the manager but this is good for a demo
+    void Update(){
+        input();
+
     }
 
     void Attack() {
